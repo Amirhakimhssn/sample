3bb3e996e0bc142248764efe86716dac0a758d39
Amirhakimhssn <107099195+Amirhakimhssn@users.noreply.github.com>
2022-06-08 15:59:16 +0800

    Update abcd.py

--- abcd.py
+++ abcd.py
@@ -1,3 +1,6 @@
+import streamlit as st
+import pandas as pd
+
 st.header("My first Streamlit App")
 
 st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
